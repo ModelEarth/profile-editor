39eac9b9b83b337acbdb593127364949b500d6b2
modelearth <model@dreamstudio.com>
2021-11-05 11:00:20 -0400

    Title: Profile Editor

--- src/app/components/head.js
+++ src/app/components/head.js
@@ -50,7 +50,7 @@ class head extends Component {
     let { info } = this.state;
     return (
       <div className="content__head">
-        <div className="content__head__title">publiccode.yml Editor</div>
+        <div className="content__head__title">Profile Editor</div>
         <div className="content__head__help">
           <div>
             <a className="pr-5" href={privacyPolicyUrl} rel="noopener noreferrer" target="_blank">
